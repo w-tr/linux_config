c29fbad9c42793af9e4a394c47548aec65390a66
wtr <9586974+w-tr@users.noreply.github.com>
2024-12-09 21:30:16 +0100

    Add terminal config files.

---
+++ .wezterm.lua
@@ -0,0 +1,30 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("UbuntuMono Nerd Font Mono")
+config.font_size = 14
+
+-- config.enable_wayland = true
+-- config.use_system_cursor = true
+-- keep adding config stuff below
+--
+config.leader = {
+  key = 'a',
+  mods = 'CTRL',
+  timeout_milliseconds = 2000,
+}
+config.keys = {
+  {
+    key = '[',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateCopyMode,
+  }, {
+  key = 'f',
+  mods = 'ALT',
+  action = wezterm.action.TogglePaneZoomState,
+},
+}
+return config
