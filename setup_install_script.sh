34342817e8e17ea76086dbfb2c0c456ed163be2d
wtr <9586974+w-tr@users.noreply.github.com>
2024-12-22 21:35:50 +0100

    Move away from oh my zsh for smaller build.

--- setup_install_script.sh
+++ setup_install_script.sh
@@ -1,5 +1,7 @@
 sudo apt update && sudo apt upgrade -y
+################################################################################
 ##### Get essentials
+################################################################################
 sudo apt install -y \
 	neovim \
 	git \
@@ -10,12 +12,17 @@ sudo apt install -y \
 	meld \
 	gtkterm \
 	gtkhash \
-	gimp
+	gimp \
+	ripgrep
 
+################################################################################
 ##### Give permissions
+################################################################################
 sudo usermod -aG lp,dialout $(whoami)
 
+################################################################################
 ##### Install vscode as per code.vis`ualstudio.com/doc...
+################################################################################
 sudo apt-get install -y gpg software-properties-common apt-transport-https
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
@@ -24,24 +31,26 @@ rm -f packages.microsoft.gpg
 sudo apt update
 sudo apt install code
 
-##### Install WezTerm
+################################################################################
+##### Install WezTerm with zsh & nerd font
+################################################################################
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt update
 sudo apt install wezterm
-# Move away from Bash and embrace oh my zsh setup.
+## Move away from Bash and embrace oh my zsh setup.
 sudo apt install zsh
+# Oh my zsh is a bloated plugin manager.
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+## Install nerd font(s)
 sudo apt install powerline-fonts
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-##### Install nerd font(s)
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh UbuntuMono
 cd ..
 rm -rf nerd-fonts
-
-##### A search that works.
+## A search that works.
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+chsh -s $(which zsh)
