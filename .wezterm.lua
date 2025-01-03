6c0abf2a9e4ef908f41964f47ac9cf26127ed39a
wtr <9586974+w-tr@users.noreply.github.com>
2025-01-03 23:36:01 +0100

    Configure Wezterm to be a bit prettier.

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,10 +6,27 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("UbuntuMono Nerd Font Mono")
 config.font_size = 14
+config.window_background_opacity = 0.9 -- Consider blur.
+-- config.macos_window_background_blur = 20
+config.text_background_opacity = 1.0
+config.use_fancy_tab_bar = true
+config.colors = {
+	tab_bar = {
+		active_tab =  {
+			bg_color = "#7A479E",
+			fg_color = "#ebdbb2",
+		},
+		inactive_tab = {
+			bg_color = "#3c3836",
+			fg_color = "#ebdbb2",
+		},
+	},
+	background = "#000000"
+ }
 
 -- config.enable_wayland = true
 -- config.use_system_cursor = true
--- keep adding config stuff below
+
 --
 config.leader = {
 	key = "\\",
